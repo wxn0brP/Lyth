6ea1791712fa38e4a9bca1d72dd0971f4803075b
wxn0brP <toja.filiper@gmail.com>
2025-12-26 16:33:45 +0100

    refactor: help

--- src/help.ts
+++ src/help.ts
@@ -1,32 +1,32 @@
 import { readFileSync } from "fs";
 
 export default async function () {
-    const { version } = JSON.parse(readFileSync(import.meta.dirname + "/../package.json", "utf-8"));
-    const str = `
+  const { version } = JSON.parse(readFileSync(import.meta.dirname + "/../package.json", "utf-8"));
+  const str = `
 Usage: lyth <command> [args]
 Version: ${version}
 
 Commands:
-    name                                    Install a package
-        -f                                  Force install (Overwrite existing files)
-        -N                                  Don't refresh repositories
+  name                                  Install a package
+    -f                                  Force install (overwrite existing files)
+    -N                                  Don't refresh repositories
 
-    -R, rm                                  Uninstall a package
-    update, up                              Update all packages
-    list, ls                                List installed packages
-    search, s                               Search for packages
-    serve, gui                              Start Lyth GUI
+  -R, rm                                Uninstall a package
+  update, up                            Update all packages
+  list, ls                              List installed packages
+  search, s                             Search for packages
+  serve, gui                            Start Lyth GUI
 
-    repo                                    Repository management
-        add <name> <url>                    Add a repository
-        rm <name>                           Remove a repository
-        list                                List repositories
-        pull <name>                         Update a repository
-        pull-all, pa                        Update all repositories
-        create-meta <name>                  Add existing repository to database
+  repo                                  Repository management
+    add <name> <url>                    Add a repository
+    rm <name>                           Remove a repository
+    list                                List repositories
+    pull <name>                         Update a repository
+    pull-all, pa                        Update all repositories
+    create-meta <name>                  Add existing repository to database
 
-    update-self                             Update lyth
-    -v, --version                           Show version
+  update-self                           Update lyth
+  -v, --version                         Show version
 `;
-    console.log(str.trim());
+  console.log(str.trim());
 }
